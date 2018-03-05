3ac1ecec3300f7add9f3a64d9ec13c513259d5f4
Ravindra R. Jaju <jaju@msync.org>
2018-03-05 22:06:55 +0530

    Current version commit

--- personal/aboveall.el
+++ personal/aboveall.el
@@ -1,7 +1,18 @@
 ;; From https://github.com/stuartsierra/dotfiles
 
-;;(require 'smex)
-;;(smex-initialize)
+(require 'smex)
+(smex-initialize)
+
+(global-set-key (kbd "H-<up>") 'previous-line)
+(global-set-key (kbd "H-<down>") 'next-line)
+(global-set-key (kbd "H-<right>") 'forward-char)
+(global-set-key (kbd "H-<left>") 'backward-char)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; The original M-x
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; Switch Windows like tabs
 (global-set-key (kbd "M-<tab>") 'other-window)
@@ -11,8 +22,8 @@
 
 (require 'ox-reveal)
 
-(eval-after-load 'ox ;; shouldn't be byte compiled.
-  '(when (and user-init-file (buffer-file-name)) ;; don't do it in async
-     (setq org-export-async-init-file
-           (expand-file-name "init-org-async.el" (file-name-directory
-                                                  user-init-file)))))
+;(eval-after-load 'ox ;; shouldn't be byte compiled.
+;  '(when (and user-init-file (buffer-file-name)) ;; don't do it in async
+;     (setq org-export-async-init-file
+;           (expand-file-name "init-org-async.el" (file-name-directory
+;                                                  user-init-file)))))
--- personal/orgmode.el
+++ personal/orgmode.el
@@ -2,20 +2,12 @@
 
 ;; Author: Ravindra Jaju - https:/msync.org/
 
-;;; Code:
-
 (require 'org)
-(require 'ox-hugo)
+(setq org-agenda-files '("~/.org/agenda" "~/.org/notes"))
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-
-(define-key global-map (kbd "<f9>") (lambda () (interactive) (cd "~/.org/blog")))
-(define-key global-map (kbd "<f10>") (lambda () (interactive) (org-publish-project "msync")))
-(define-key global-map (kbd "<f11>") (lambda () (interactive) (org-publish-project "msync-presentation")))
-
-(setq org-agenda-files '("~/.org/agenda" "~/.org/notes"))
 (setq org-log-done t) ;; This sets timestamps on tasks when finished.
 (setq org-startup-indented t)
 (setq org-src-fontify-natively t)
@@ -31,42 +23,34 @@
 (require 'cider)
 (setq org-babel-clojure-backend 'cider)
 
-;; And Python
+;; And Python, JS
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
+ '((python . t)
+   (js . t)))
+
+
 
 ;; More from http://fgiasson.com/blog/index.php/2016/04/05/using-clojure-in-org-mode-and-implementing-asynchronous-processing/
-(org-defkey org-mode-map "\C-x\C-e" 'cider-eval-last-sexp)
-(org-defkey org-mode-map "\C-c\C-d" 'cider-doc)
+;(org-defkey org-mode-map "\C-x\C-e" 'cider-eval-last-sexp)
+;(org-defkey org-mode-map "\C-c\C-d" 'cider-doc)
 ;; END
 
+(require 'ox-publish)
+(require 'ox-hugo)
 (setq org-html-html5-fancy t)
 (setq org-src-tab-acts-natively t)
 
-(setq org-reveal-root "/js/reveal.js")
+(setq org-reveal-root "http://p.msync.org/reveal.js")
 (define-skeleton org-presentation-skeleton
   "Inserts org directives for org buffers supposed to generate a slide-show presentation, using reveal.js"
   "Title: "
   "#+Title: " str "\n"
   "#+Author: Ravindra R. Jaju\n"
   "#+Email: first-name -dot- last-name -at- the-world's-email-domain\n"
+  "#+FILETAGS: :sometag:\n"
+  "#+SETUPFILE: ~/.org/presentations/settings.org\n"
   "\n"
-  "#+TAGS: example(e) video(v) discuss(d) improve(i) fixme(f) quiz(q) exercise(x)\n"
-  "#+OPTIONS: reveal_center:t reveal_progress:t reveal_history:nil reveal_control:t\n"
-  "#+OPTIONS: reveal_rolling_links:t reveal_keyboard:t reveal_overview:t num:nil\n"
-  "#+OPTIONS: reveal_width:1200 reveal_height:800\n"
-  "#+OPTIONS: toc:1\n"
-  "#+REVEAL_MARGIN: 0.1\n"
-  "#+REVEAL_MIN_SCALE: 0.5\n"
-  "#+REVEAL_MAX_SCALE: 2.5\n"
-  "#+REVEAL_TRANS: cube\n"
-  "#+REVEAL_THEME: moon\n"
-  "#+REVEAL_HLEVEL: 2\n"
-  "#+REVEAL_HEAD_PREAMBLE: <meta name='description' content=''>\n"
-  "#+REVEAL_POSTAMBLE: <p>Created by Ravindra R. Jaju.</p>\n"
-  "#+REVEAL_PLUGINS: (markdown notes)\n"
-  "#+REVEAL_EXTRA_CSS: ./local.css\n"
   )
 
 (define-skeleton org-slide-skeleton
@@ -83,8 +67,22 @@
   "\n"
   "#+END_NOTES")
 
-;; For jekyll publishing - blog.msync.in
-(require 'ox-publish)
+(setq hugo-base-dir "~/Projects/hugo-blog")
+(define-skeleton org-blog-skeleton
+  "Inserts the right directives for hugo-orgmode blogging"
+  "Title: "
+  "#+HUGO_BASE_DIR: " hugo-base-dir "\n"
+  "#+HUGO_SECTION: posts/" (format-time-string "%Y") "\n"
+  "#+AUTHOR: Ravindra R. Jaju\n"
+  "#+OPTIONS: toc:nil num:nil\n"
+  "\n"
+  "#+HUGO_TAGS: []\n"
+  "#+Title: " str "\n"
+  "#+HUGO_CUSTOM_FRONT_MATTER: :abstract <Modify me>\n"
+  "#+date: " (now) "\n"
+  "#+lastmod: " (now) "\n"
+  "#+published: false\n"
+  "\n")
 
 (setq org-publish-project-alist
       '(
@@ -117,24 +115,6 @@
          :headline-levels 4
          :publishing-function org-reveal-export-to-html)))
 
-(define-skeleton org-blog-skeleton
-  "Inserts the right directives for hugo-orgmode blogging"
-  "Title: "
-  "#+OPTIONS: toc:nil num:nil\n"
-  "#+TITLE: " str "\n"
-  "author: Ravindra R. Jaju\n"
-  "abstract:\n"
-  "date: " (now) "\n"
-  "lastmod:\n"
-  "tags: []\n"
-  "published: false\n"
-  "---\n"
-  "#+END_HTML\n\n"
-  "#+INFOJS_OPT: path:/js/org-info.js toc:nil ltoc:t up:/ home:/\n"
-  "; view options are - info, overview, content, showall\n"
-  "#+INFOJS_OPT: view:content\n"
-  "\n")
-
 (custom-set-variables
  '(org-display-custom-times t)
  '(org-time-stamp-custom-formats "%Y-%m-%dT%T%:z"))
@@ -144,6 +124,7 @@
   (insert (format-time-string "%Y-%m-%dT%T%:z")))
 (global-set-key (kbd "C-c C-t .") 'now)
 
+(define-key global-map (kbd "<f9>") (lambda () (interactive) (org-hugo-export-to-md)))
 
 ;(eval-after-load 'ox ;; shouldn't be byte compiled.
 ;		 '(when (and user-init-file (buffer-file-name)) ;; don't do it in async
